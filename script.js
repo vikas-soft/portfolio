48a5e80230d4c4fde3c27090dd63e15135b7c872
vikas kumar <75654548+vikas-soft@users.noreply.github.com>
2024-10-22 14:48:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
   hamburger.classList.toggle('toggle');
 });
+
+//preloader
+var loader=document.getElementById("preloader");
+window.addEventListener("load",function(){
+  loader.style.display='none';
+})
